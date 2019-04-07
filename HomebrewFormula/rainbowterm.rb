82481a55035549cb5be5c6499ff8e352734a014c
Mitchell Kember <mk12360@gmail.com>
2019-04-07 14:57:19 -0400

    Add sha256 to formula

--- HomebrewFormula/rainbowterm.rb
+++ HomebrewFormula/rainbowterm.rb
@@ -2,12 +2,13 @@ class Rainbowterm < Formula
   desc "Color theme manager for iTerm2"
   homepage "https://github.com/mk12/rainbowterm"
   url "https://github.com/mk12/rainbowterm/archive/0.1.0.tar.gz"
+  sha256 "bf11151dc2606a49dfa97117b45ef1b575a03c111f9c15a6c5a79b3229b48959"
   head "https://github.com/mk12/rainbowterm.git"
 
   depends_on "python"
 
   def install
-    system "cp", "rainbowterm.py", "rainbowterm"
+    cp "rainbowterm.py", "rainbowterm"
     bin.install "rainbowterm"
   end
 
