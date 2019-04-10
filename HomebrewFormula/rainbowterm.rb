c52df8451d6c162ec47b40ac413145298de8c00c
Mitchell Kember <mk12360@gmail.com>
2019-04-09 20:55:22 -0400

    Add setup.py and use virtualenv in formula

--- HomebrewFormula/rainbowterm.rb
+++ HomebrewFormula/rainbowterm.rb
@@ -1,15 +1,29 @@
 class Rainbowterm < Formula
-  desc "Color scheme manager for iTerm2"
+  include Language::Python::Virtualenv
+
+  desc "Tool for managing color schemes in iTerm2"
   homepage "https://github.com/mk12/rainbowterm"
   url "https://github.com/mk12/rainbowterm/archive/0.2.0.tar.gz"
   sha256 "48e50d1ff15540df3d143e7fce99bcf541880dac1105ce3d064c20f5d66b002e"
   head "https://github.com/mk12/rainbowterm.git"
 
   depends_on "python"
+  depends_on "brightness" => :recommended
+  depends_on "locateme" => :recommended
+  depends_on "fzf" => :optional
+
+  resource "astral" do
+    url "https://files.pythonhosted.org/packages/86/05/25c772065bb6384789ca0f6ecc9d0bdd0bc210064e5c78453ee15124082e/astral-1.10.1.tar.gz"
+    sha256 "d2a67243c4503131c856cafb1b1276de52a86e5b8a1d507b7e08bee51cb67bf1"
+  end
+
+  resource "dateutil" do
+    url "https://files.pythonhosted.org/packages/ad/99/5b2e99737edeb28c71bcbec5b5dda19d0d9ef3ca3e92e3e925e7c0bb364c/python-dateutil-2.8.0.tar.gz"
+    sha256 "c89805f6f4d64db21ed966fda138f8a5ed7a4fdbc1a8ee329ce1b74e3c74da9e"
+  end
 
   def install
-    cp "rainbowterm.py", "rainbowterm"
-    bin.install "rainbowterm"
+    virtualenv_install_with_resources
   end
 
   test do
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="rainbowterm",
+    version="0.2.0",
+    description="Tool for managing color schemes in iTerm2",
+    py_modules=["rainbowterm"],
+    entry_points={"console_scripts": ["rainbowterm = rainbowterm:main"]},
+    depends=["astral", "dateutil"],
+    url="https://github.com/mk12/rainbowterm",
+)
