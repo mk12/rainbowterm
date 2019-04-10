3e2b9240fa5c635b94dcd35b0b975f45b8c7c0f6
Mitchell Kember <mk12360@gmail.com>
2019-04-10 09:36:48 -0400

    Bump version to 0.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="rainbowterm",
-    version="0.3.0",
+    version="0.4.0",
     description="Tool for managing color schemes in iTerm2",
     py_modules=["rainbowterm"],
     entry_points={"console_scripts": ["rainbowterm = rainbowterm:main"]},
