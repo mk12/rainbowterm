c22d9f550bbeab114dcd40f849e2aaa637e30001
Mitchell Kember <mk12360@gmail.com>
2019-04-09 21:04:37 -0400

    Bump version to 0.3.0

--- HomebrewFormula/rainbowterm.rb
+++ HomebrewFormula/rainbowterm.rb
@@ -3,7 +3,7 @@ class Rainbowterm < Formula
 
   desc "Tool for managing color schemes in iTerm2"
   homepage "https://github.com/mk12/rainbowterm"
-  url "https://github.com/mk12/rainbowterm/archive/0.2.0.tar.gz"
+  url "https://github.com/mk12/rainbowterm/archive/0.3.0.tar.gz"
   sha256 "48e50d1ff15540df3d143e7fce99bcf541880dac1105ce3d064c20f5d66b002e"
   head "https://github.com/mk12/rainbowterm.git"
 
